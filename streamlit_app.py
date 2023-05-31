c58615fa7c75b4c2c952a92eee0930f2d58babb1
tanjusay <112522489+tanjusay@users.noreply.github.com>
2023-05-31 15:06:08 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,78 +1,66 @@
 import streamlit as st
 
-import asyncio
+import whisper
 
-import websockets
+# Load the Whisper model
 
-import numpy as np
+model = whisper.load_model("base")
 
-import sounddevice as sd
+# Streamlit app title and header
+
+st.title("Whisper Transcription App")
+
+st.header("Transcribe Audio to Text")
+
+# File uploader for audio input
+
+audio_file = st.file_uploader("Upload an audio file", type=["mp3", "wav", "ogg"])
+
+# Button to transcribe audio
+
+if st.button("Transcribe"):
+
+    if audio_file is not None:
+
+        # Load the audio file
+
+        audio_data = audio_file.read()
+
+        
+
+        # Perform transcription
+
+        result = model.transcribe(audio_data)
+
+        
+
+        # Get the transcribed text
+
+        transcribed_text = result["text"]
+
+        
+
+        # Display the transcribed text
+
+        st.subheader("Transcribed Text:")
+
+        st.write(transcribed_text)
+
+        
+
+        # Save transcribed text to file
+
+        with open("transcription.txt", "w") as f:
+
+            f.write(transcribed_text)
+
+            
+
+        st.success("Transcription complete and saved to 'transcription.txt'")
+
+    else:
+
+        st.warning("Please upload an audio file to transcribe")
 
 
-
-st.title("Whisper Transcriber")
-
-# Constants for audio recording
-
-duration = 10  # Recording duration in seconds
-
-sample_rate = 16000  # Audio sample rate
-
-
-
-async def transcribe_audio(audio_frames):
-
-    async with websockets.connect('wss://whisper-asr.api.cloud.openai.com/speech-to-text/v1/whisper/asr/streaming') as websocket:
-
-        for frame in audio_frames:
-
-            await websocket.send(frame.tobytes())
-
-            result = await websocket.recv()
-
-            # Process and display the result as needed
-
-            st.write(result)
-
-# Streamlit app content
-
-with st.beta_container():
-
-    recording = st.empty()
-
-    if st.button("Start/Stop Recording"):
-
-        if recording.button_value:
-
-            recording.button_value = False
-
-            recording.empty()
-
-        else:
-
-            recording.button_value = True
-
-            recording_text = st.empty()
-
-            audio_frames = []
-
-            def callback(indata, frames, time, status):
-
-                audio_frames.append(indata.copy())
-
-            with sd.InputStream(callback=callback, channels=1, samplerate=sample_rate):
-
-                st.write("Recording started. Click 'Start/Stop Recording' to stop.")
-
-                while recording.button_value:
-
-                    pass
-
-            st.write("Recording stopped. Transcribing audio...")
-
-            audio_data = np.concatenate(audio_frames)
-
-            loop = asyncio.get_event_loop()
-
-            loop.run_until_complete(transcribe_audio(audio_data))
-
+            
