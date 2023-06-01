bc32649d8fa84a5759d7abe77a3d3a0e136cbca6
tanjusay <112522489+tanjusay@users.noreply.github.com>
2023-06-01 19:54:51 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,23 +5,22 @@ import streamlit as st
 def main():
     st.set_page_config(page_title="AI Transcriber App")
     st.header("🪶 AI Transcriber App")
-    
+
     model = whisper.load_model("base")
-    
-    file = st.file_uploader("Upload a video file", type=["mp4"])
-    
+
+    file = st.file_uploader("Upload an MP4 file", type=["mp4"])
+
     if file is not None:
-        # Transcribe the video
         result = model.transcribe(file)
-        
-        # Save the transcription to a text file
+
         with open("transcription.txt", "w") as f:
             f.write(result["text"])
-        
-        st.success("Transcription completed and saved to transcription.txt")
-    else:
-        st.info("Please upload a video file (.mp4)")
-        
+
+        st.subheader("Transcribed Text:")
+        with open("transcription.txt", "r") as f:
+            transcribed_text = f.read()
+            st.write(transcribed_text)
+
 
 if __name__ == "__main__":
     main()
